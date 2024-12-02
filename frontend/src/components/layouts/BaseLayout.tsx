6e724b11bd343751eab1400a85090deb5108d6d6
Bladimir Cabrera <vladwithcode@gmail.com>
2024-12-01 23:37:22 -0600

    fix: Use store open state to control sidebar

--- frontend/src/components/layouts/BaseLayout.tsx
+++ frontend/src/components/layouts/BaseLayout.tsx
@@ -7,7 +7,7 @@ import { useSidebarStore } from "../../stores/useSidebarStore";
 export default function BaseLayout({ children }: PropsWithChildren) {
     const sidebarStore = useSidebarStore()
     return (
-        <SidebarProvider defaultOpen={false}>
+        <SidebarProvider open={sidebarStore.open} defaultOpen={false}>
             <AppSidebar />
             <div className="relative flex flex-col w-full h-full z-0 overflow-scroll p-2">
                 {children !== undefined ? children : <Outlet />}
