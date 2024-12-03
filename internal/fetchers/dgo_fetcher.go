6e90ca5586e13e8879704725db26fd7986c2f030
Bladimir Cabrera <vladwithcode@gmail.com>
2024-12-02 20:40:26 -0600

    fix: pdftotext command fails to execute

--- internal/fetchers/dgo_fetcher.go
+++ internal/fetchers/dgo_fetcher.go
@@ -7,25 +7,24 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 	"os/exec"
 	"time"
 )
 
-const LARGE_DATA_SIZE = 65_000
 const DGO_URLF = "http://tsjdgo.gob.mx/Recursos/images/flash/ListasAcuerdos/%v/%v.pdf"
+const LARGE_DATA_SIZE = 60_000
 
 func DgoFetch(date time.Time, caseType string) (data *[]byte, err error) {
 	data, err = dgoFetchResource(date, caseType)
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Fetch file err: %w", err)
 	}
 
-	err = dataToPDF(data)
+	err = PDFToData(data)
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Transform file err: %w", err)
 	}
 
 	return
@@ -46,6 +45,7 @@ func dgoFetchResource(date time.Time, caseType string) (data *[]byte, err error)
 		return nil, errors.New("No se encontró documento para la fecha solicitada")
 	}
 
+	data = new([]byte)
 	*data, err = io.ReadAll(response.Body)
 
 	if err != nil {
@@ -55,68 +55,86 @@ func dgoFetchResource(date time.Time, caseType string) (data *[]byte, err error)
 	return data, nil
 }
 
-func dataToPDF(data *[]byte) error {
-	if len(*data) >= LARGE_DATA_SIZE {
-		return largeDataToPDF(data)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-	transformCmd := exec.CommandContext(ctx, "pdftotext", "-fixed 5", "-", "-")
-	inPipe, err := transformCmd.StdinPipe()
-	defer inPipe.Close()
-
-	if err != nil {
-		return err
-	}
-
-	_, err = inPipe.Write(*data)
-
-	if err != nil {
-		return err
-	}
-
-	*data, err = transformCmd.Output()
-
-	return nil
-}
-
-func largeDataToPDF(data *[]byte) error {
-	inRead, inWrite, _ := os.Pipe()
-	outRead, outWrite, _ := os.Pipe()
+func PDFToData(data *[]byte) error {
 	outBuf := new(bytes.Buffer)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer cancel()
 
-	transformCmd := exec.CommandContext(ctx, "pdftotext", "-fixed 5", "-", "-")
-	transformCmd.Stdin = inRead
-	transformCmd.Stdout = outWrite
+	transformCmd := exec.CommandContext(ctx, "pdftotext", "-fixed", "5", "-", "-")
+
+	stdIn, err := transformCmd.StdinPipe()
+	if err != nil {
+		return fmt.Errorf("Error getting stdin pipe:\n\t%w", err)
+	}
+
+	stdOut, err := transformCmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("Error getting stdout pipe:\n\t%w", err)
+	}
 
 	if err := transformCmd.Start(); err != nil {
-		return err
+		return fmt.Errorf("Start cmd error: %w", err)
 	}
 
 	// Concurrenty writes to cmd in pipe
 	go func() {
-		defer inWrite.Close()
-
-		inWrite.Write(*data)
+		defer stdIn.Close()
+		stdIn.Write(*data)
 	}()
 
 	// Councurrently reads from cmd out pipe
 	go func() {
-		defer outRead.Close()
-
-		outBuf.ReadFrom(outRead)
+		defer stdOut.Close()
+		outBuf.ReadFrom(stdOut)
 	}()
 
 	// Await read, write and execution of the command
 	if err := transformCmd.Wait(); err != nil {
-		return nil
+		return fmt.Errorf("Error executing pdftotext:\n  %w", err)
 	}
 
 	*data = outBuf.Bytes()
+
+	return nil
+}
+
+// Keeping this for reference. It may not be usefull at all in the future
+func _pdfToData(data *[]byte) error {
+	if len(*data) >= LARGE_DATA_SIZE {
+		fmt.Printf("PDF of size %v. Using largePDFToData handler\n", len(*data))
+		return PDFToData(data)
+	}
+
+	transformCmd := exec.Command("pdftotext", "-fixed", "5", "-", "-")
+
+	stdIn, err := transformCmd.StdinPipe()
+	if err != nil {
+		return fmt.Errorf("Error getting stdin pipe:\n\t%w", err)
+	}
+	stdOut, err := transformCmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("Error getting stdout pipe:\n\t%w", err)
+	}
+
+	if err := transformCmd.Start(); err != nil {
+		return fmt.Errorf("Error starting cmd:\n\t%w", err)
+	}
+
+	out := make([]byte, len(*data))
+	go func() {
+		defer stdIn.Close()
+		stdIn.Write(*data)
+	}()
+	go func() {
+		stdOut.Read(out)
+	}()
+
+	if err := transformCmd.Wait(); err != nil {
+		return fmt.Errorf("Error executing cmd:\n\t%w", err)
+	}
+
+	*data = out
 
 	return nil
 }
---
+++ internal/readers/dgo_reader_test.go
@@ -0,0 +1,13 @@
+package readers
+
+import (
+	"testing"
+)
+
+func TestDgoReader(t *testing.T) {
+	input := `Header Line
+More useless data
+
+No.		
+	`
+}
