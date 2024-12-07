3d123e2c4b53b6dfcd2d236576b69fc83349d201
Bladimir Cabrera <vladwithcode@gmail.com>
2024-12-07 00:07:16 -0600

    maintenance: simplify connection string

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,14 +2,13 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/pkg/errors"
 )
 
-const DEFAULT_CONN_STR = "data/lex_app.db"
+const DEFAULT_CONN_STR = "./data/lex_app.db"
 
 var connStr = os.Getenv("LEX_DB_URL")
 
@@ -17,10 +16,8 @@ var db *sql.DB
 
 func Connect() (db *sql.DB, err error) {
 	if connStr == "" {
-		wd, _ := os.Getwd()
-		connStr = wd + "/" + DEFAULT_CONN_STR
+		connStr = DEFAULT_CONN_STR
 	}
-	fmt.Printf("connStr: %v\n", connStr)
 	db, err = sql.Open("sqlite3", connStr)
 	if err != nil {
 		return nil, err
