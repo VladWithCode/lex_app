dd53908ad29d7009265bb5a6ac8ec398e1f1d0cc
Bladimir Cabrera <vladwithcode@gmail.com>
2024-12-13 06:09:37 -0600

    maintenance: Setup double click handler for titlebar

--- frontend/src/components/TitleBar.tsx
+++ frontend/src/components/TitleBar.tsx
@@ -1,11 +1,22 @@
+import { MouseEvent } from "react";
 import AppControls from "./AppControls";
+import { WindowIsMaximised, WindowMaximise, WindowUnmaximise } from "../../wailsjs/runtime/runtime";
+
+async function onDbClick(e: MouseEvent<HTMLDivElement>) {
+    e.preventDefault()
+    if (await WindowIsMaximised()) {
+        WindowUnmaximise()
+    } else {
+        WindowMaximise()
+    }
+}
 
 export default function TitleBar() {
     return (
         <div
             style={{ "--wails-draggable": "drag" } as React.CSSProperties}
-            className="relative top-0 left-0 right-0 w-full h-8 flex px-2 hover:cursor-grab focus:cursor-grabbing active:cursor-grabbing"
-            >
+            className="relative top-0 left-0 right-0 w-full h-8 flex px-2 hover:cursor-grab focus:cursor-grabbing active:cursor-grabbing select-none"
+            onDoubleClick={onDbClick}>
             <div className="flex items-center">
                 <p className="ml-2 text-stone-400 font-semibold">lexApp</p>
             </div>
