5d9942df95ed4891d07b6b35878a69f11ef7271f
Bladimir Cabrera <vladwithcode@gmail.com>
2024-12-12 20:18:06 -0600

    maintenance: use tailwindcss v4.0 beta

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 const srcPath = new URL('./src', import.meta.url).pathname
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
       alias: {
           "@": srcPath,
