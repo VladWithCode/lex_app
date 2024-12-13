9218365c062b56d6f7ea91dcaff1da371e05c7df
Bladimir Cabrera <vladwithcode@gmail.com>
2024-12-12 20:16:46 -0600

    feat: Case display components

---
+++ frontend/src/components/cases/CaseCard.tsx
@@ -0,0 +1,36 @@
+import { LucideEye } from "lucide-react";
+import { Button } from "../ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../ui/card";
+import { Separator } from "../ui/separator";
+import { db } from "../../../wailsjs/go/models"
+import { CaseType, caseTypeToName } from "../../lib/caseTypeNames";
+
+export type CaseCardProps = React.PropsWithChildren & {
+    caseData: db.lexCase
+}
+
+export default function CaseCard({ caseData }: CaseCardProps) {
+    return (
+        <Card className="bg-zinc-900 basis-1/3 flex-grow-0 flex-shrink-0 overflow-hidden">
+            <CardHeader className="p-3">
+                <CardTitle className="text-lg font-medium">Expediente {caseData.caseId} | {caseTypeToName(caseData.caseType as CaseType)}</CardTitle>
+            </CardHeader>
+            <Separator />
+            <CardContent className="space-y-2 px-3 py-1.5 pb-3">
+                <p className="text-stone-400 font-semibold">Ultimo acceso: {caseData.lastAccessedAt}</p>
+                <p className="text-stone-200">
+                    EJECUTIVO MERCANTIL PRESTAMOS FELICES EN 15
+                    MINUTOS SOCIEDAD...
+                </p>
+            </CardContent>
+            <Separator />
+            <CardFooter className="p-0">
+                <Button
+                    className="relative z-0 flex text-stone-50 bg-zinc-900 hover:bg-stone-800 rounded-none justify-center flex-grow basis-1/3 max-w-1/2 py-1 [&_svg]:size-6 group">
+                    <span className="absolute text-lg font-bold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-[opacity] duration-300 opacity-0 group-hover:opacity-100">Ver</span>
+                    <LucideEye className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 stroke-current transition-[opacity] duration-300 opacity-100 group-hover:opacity-0" />
+                </Button>
+            </CardFooter>
+        </Card>
+    )
+}
---
+++ frontend/src/components/cases/CaseListing.tsx
@@ -0,0 +1,24 @@
+import CaseCard from "./CaseCard"
+import { useCases } from "../../queries/cases"
+import { LucideLoader } from "lucide-react"
+
+export default function CaseListing() {
+    const { data, isSuccess, isLoading, isError } = useCases({ limit: 5 })
+
+    if (isError) {
+        return <div className="flex h-36 items-center justify-center">
+            <p className="text-stone-200 text-xl font-semibold">Ocurrio un error al recuperar los casos</p>
+        </div>
+    }
+
+    if (isLoading) {
+        return <div className="flex flex-col h-36 items-center justify-center">
+            <LucideLoader className="animate-spin" />
+            <p className="text-lg text-stone-400 uppercase font-semibold">Cargando Casos Recientes...</p>
+        </div>
+    }
+
+    return <div className="flex gap-4 max-w-full overflow-visible mb-2">
+        {isSuccess && data.map(c => <CaseCard key={c.id} caseData={c} />)}
+    </div>
+}
