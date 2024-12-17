19b2a1cf081ba4b448185b85d4036f87aa28a5f2
Bladimir Cabrera <vladwithcode@gmail.com>
2024-12-16 20:58:16 -0600

    feat: AccordUpdater FindFiles impl

--- internal/accupdter/accord_updater.go
+++ internal/accupdter/accord_updater.go
@@ -1,6 +1,7 @@
 package accupdter
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -37,19 +38,100 @@ type UpdatedAccord struct {
 }
 
 type BasicAccUpdter struct {
-	fetch fetchers.Fetcher
-	read  readers.Reader
+	Fetch fetchers.Fetcher
+	Read  readers.Reader
 	opts  *AccUpdterOpts
 }
 
 func NewBasicUpdter(opts *AccUpdterOpts) *BasicAccUpdter {
 	return &BasicAccUpdter{
-		fetch: fetchers.NewFetcher(opts.Region),
-		read:  readers.NewReader(opts.Region),
+		Fetch: fetchers.NewFetcher(opts.Region),
+		Read:  readers.NewReader(opts.Region),
 		opts:  opts,
 	}
 }
 
+// Only fetches and reads, returning results
+func (updter *BasicAccUpdter) FindUpdates() (updatedAccords []*UpdatedAccord, notFoundIds []string, err error) {
+	searchDate := time.Now()
+	updatedAccords = []*UpdatedAccord{}
+	notFoundIds = make([]string, len(updter.opts.CaseIds))
+	copy(notFoundIds, updter.opts.CaseIds)
+
+	if updter.opts.SearchStartDate != (time.Time{}) {
+		searchDate, _ = time.Parse(
+			"2006-01-02",
+			updter.opts.SearchStartDate.Format("2006-01-02"),
+		)
+	}
+
+	for i := 0; i <= updter.opts.MaxSearchBack; i++ {
+		data, err := updter.Fetch(searchDate, updter.opts.CaseType)
+		if err != nil {
+			if errors.Is(err, fetchers.ErrDocNotFound) {
+				continue
+			}
+
+			return nil, nil, fmt.Errorf(
+				"Error FetchFail: with SearchStartDate: %s; CaseType %s; Region: %s\n%w",
+				updter.opts.SearchStartDate.Format("2006-01-02"),
+				updter.opts.CaseType,
+				updter.opts.Region,
+				err,
+			)
+		}
+
+		caseTable, err := updter.Read(data)
+		if err != nil {
+			return nil, nil, fmt.Errorf(
+				"Error ReadFail: with SearchStartDate: %s; CaseType %s; Region: %s\n  %w",
+				updter.opts.SearchStartDate.Format("2006-01-02"),
+				updter.opts.CaseType,
+				updter.opts.Region,
+				err,
+			)
+		}
+
+		nextSearchIds := []string{}
+		for _, id := range notFoundIds {
+			if caseRow := caseTable.Find(id); caseRow != nil {
+				acc := UpdatedAccord{
+					CaseType: internal.CaseType(caseRow.CaseType),
+					CaseId:   caseRow.CaseId,
+					Content:  caseRow.Accord,
+					Date:     searchDate,
+					Nature:   caseRow.Nature,
+					OthIds:   caseRow.AllIds,
+				}
+				updatedAccords = append(updatedAccords, &acc)
+				continue
+			}
+			nextSearchIds = append(nextSearchIds, id)
+		}
+
+		if len(nextSearchIds) == 0 {
+			break
+		}
+
+		notFoundIds = nextSearchIds
+		searchDate = searchDate.Add(-24 * time.Hour)
+	}
+
+	return updatedAccords, notFoundIds, nil
+}
+
 // Doesn't return but updates DB with accords found
 func (updter *BasicAccUpdter) Update() {
+}
+
+// Appends CaseRows and index entries from the mergingTable into the targetTable
+// only if the targetTable does not contain an existing row for the caseId
+//
+// The merge occurs in place
+func mergeCaseTables(targetTable, mergingTable *readers.CaseTable) {
+	for _, mRow := range mergingTable.Cases {
+		if tRow := targetTable.Find(mRow.CaseId); tRow == nil {
+			targetTable.Add(mRow)
+		}
+	}
 }
---
+++ internal/accupdter/accord_updater_test.go
@@ -0,0 +1,144 @@
+package accupdter
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/vladwithcode/lex_app/internal"
+	"github.com/vladwithcode/lex_app/internal/readers"
+)
+
+func TestFindUpdates(t *testing.T) {
+	expectAccords := []*UpdatedAccord{
+		{
+			CaseId:  "84/2003",
+			Nature:  "Some sample nature",
+			Content: "Some sample accord content",
+			OthIds:  []string{"84/2003"},
+		},
+		{
+			CaseId:  "264/2018",
+			Nature:  "Second Nature\nin 2 lines",
+			Content: "Valid content for an accord\nwith two lines of height?",
+			OthIds:  []string{"264/2018"},
+		},
+		{
+			CaseId:  "13/1998",
+			Nature:  "Third Nature",
+			Content: "This content is longer in width that previous contents, but should be stored correctly in the content col",
+			OthIds:  []string{"13/1998"},
+		},
+		{
+			CaseId:  "45/3000",
+			Nature:  "Forth Nature non\nstandard too",
+			Content: "This accord has a nature that doesn't start\non the first line",
+			OthIds:  []string{"45/3000"},
+		},
+		{
+			CaseId:  "60/1234",
+			Nature:  "Fifth with multi ID",
+			Content: "This accord has multiple caseIds",
+			OthIds:  []string{"60/1234", "60/1234-I"},
+		},
+		{
+			CaseId:  "1024/2048",
+			Nature:  "Sixth with both\nvalid/invalid id",
+			Content: "This accord has a valid and an invalid caseId",
+			OthIds:  []string{"1024/2048", "eee/wrong"},
+		},
+	}
+	expectNotFound := []string{"12/12"}
+
+	updtr := BasicAccUpdter{
+		Fetch: mockFetch,
+		Read:  readers.NewReader(internal.RegionDefault),
+		opts: &AccUpdterOpts{
+			Region:   internal.RegionDefault,
+			CaseType: internal.CaseTypeAux1,
+			CaseIds: []string{
+				"84/2003",
+				"264/2018",
+				"13/1998",
+				"45/3000",
+				"60/1234",
+				"1024/2048",
+				"12/12",
+			},
+		},
+	}
+	accords, notFound, err := updtr.FindUpdates()
+	if err != nil {
+		t.Fatalf("errored with %v", err)
+	}
+	if len(notFound) != len(expectNotFound) {
+		t.Fatalf("Expected notFoundIds to have %d ids, got %d", len(expectNotFound), len(notFound))
+	}
+	for i, expectId := range expectNotFound {
+		if expectId != notFound[i] {
+			t.Errorf("expected notFound[%d] to be %s, got %s", i, expectId, notFound[i])
+			t.Fail()
+		}
+	}
+	if len(accords) != len(expectAccords) {
+		t.Fatalf("Expected updatedAccords to have %d accords, got %d", len(expectAccords), len(accords))
+	}
+	for i, expectedAccord := range expectAccords {
+		actualAccord := accords[i]
+		if expectedAccord.CaseId != actualAccord.CaseId {
+			t.Errorf("expectedAccord[%d].CaseId is not %q, got %q", i, expectedAccord.CaseId, actualAccord.CaseId)
+			t.Fail()
+		}
+		if expectedAccord.Content != actualAccord.Content {
+			t.Errorf("expectedAccord[%d].Content is not %q, got %q", i, expectedAccord.Content, actualAccord.Content)
+			t.Fail()
+		}
+		if expectedAccord.Nature != actualAccord.Nature {
+			t.Errorf("expectedAccord[%d].Nature is not %q, got %q", i, expectedAccord.Nature, actualAccord.Nature)
+			t.Fail()
+		}
+
+		if len(expectedAccord.OthIds) != len(actualAccord.OthIds) {
+			t.Errorf("expectedAccord[%d].OthIds is not %q, got %q", i, expectedAccord.OthIds, actualAccord.OthIds)
+			t.Fail()
+		} else {
+			for j, othId := range expectedAccord.OthIds {
+				if othId != actualAccord.OthIds[j] {
+					t.Errorf(
+						"expectedAccord[%d].OthIds[%d] is not %q, got %q",
+						i,
+						j,
+						othId,
+						actualAccord.OthIds[j],
+					)
+					t.Fail()
+				}
+			}
+		}
+	}
+}
+
+func NewTestUpdater() *BasicAccUpdter {
+	return &BasicAccUpdter{
+		Fetch: mockFetch,
+		Read:  readers.NewReader(internal.RegionDefault),
+		opts: &AccUpdterOpts{
+			Region: internal.RegionDefault,
+			CaseIds: []string{
+				"84/2003",
+				"264/2018",
+				"13/1998",
+				"45/3000",
+				"60/1234",
+				"1024/2048",
+				"12/12",
+			},
+		},
+	}
+}
+
+func mockFetch(_ time.Time, _ internal.CaseType) (*[]byte, error) {
+	out, _ := os.ReadFile("./test_accord_file.txt")
+
+	return &out, nil
+}
