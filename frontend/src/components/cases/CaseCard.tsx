599c07ebd09d64c61e011642890958367aac1071
Bladimir Cabrera <vladwithcode@gmail.com>
2024-12-19 19:29:46 -0600

    maintenance: Remove view icon
    Use clear View verb for the button

--- frontend/src/components/cases/CaseCard.tsx
+++ frontend/src/components/cases/CaseCard.tsx
@@ -11,7 +11,7 @@ export type CaseCardProps = React.PropsWithChildren & {
 
 export default function CaseCard({ caseData, children }: CaseCardProps) {
     return (
-        <Card className="bg-zinc-900 basis-1/3 flex-grow-0 flex-shrink-0 overflow-hidden">
+        <Card className="bg-zinc-900 basis-1/3 flex-grow-0 flex-shrink-0 flex flex-col overflow-hidden">
             <CardHeader className="p-3">
                 <CardTitle className="text-lg font-medium">Expediente {caseData.caseId} | {caseTypeToName(caseData.caseType as CaseType)}</CardTitle>
             </CardHeader>
@@ -22,9 +22,8 @@ export default function CaseCard({ caseData, children }: CaseCardProps) {
             <Separator />
             <CardFooter className="p-0">
                 <Button
-                    className="relative z-0 flex text-stone-50 bg-zinc-900 hover:bg-stone-800 rounded-none justify-center flex-grow basis-1/3 max-w-1/2 py-1 [&_svg]:size-6 group">
-                    <span className="absolute text-lg font-bold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-[opacity] duration-300 opacity-0 group-hover:opacity-100">Ver</span>
-                    <LucideEye className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 stroke-current transition-[opacity] duration-300 opacity-100 group-hover:opacity-0" />
+                    className="flex text-stone-50 bg-zinc-900 hover:bg-stone-800 rounded-none justify-center flex-grow basis-1/3 max-w-1/2 py-1">
+                    <span className="mx-auto text-lg font-bold">Ver</span>
                 </Button>
             </CardFooter>
         </Card>
