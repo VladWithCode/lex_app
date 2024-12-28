552fea4540f69ebb15c57f30afcb10545a9c45d6
Bladimir Cabrera <vladwithcode@gmail.com>
2024-12-28 15:06:58 -0600

    maintenance: minor styling changes

--- frontend/src/components/cases/CaseCard.tsx
+++ frontend/src/components/cases/CaseCard.tsx
@@ -1,4 +1,3 @@
-import { LucideEye } from "lucide-react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../ui/card";
 import { Separator } from "../ui/separator";
@@ -13,14 +12,18 @@ export default function CaseCard({ caseData, children }: CaseCardProps) {
     return (
         <Card className="bg-zinc-900 basis-1/3 flex-grow-0 flex-shrink-0 flex flex-col overflow-hidden">
             <CardHeader className="p-3">
-                <CardTitle className="text-lg font-medium">Expediente {caseData.caseId} | {caseTypeToName(caseData.caseType as CaseType)}</CardTitle>
+                <CardTitle className="text-lg font-medium">
+                    Expediente {caseData.caseId}
+                    <br />
+                    {caseTypeToName(caseData.caseType as CaseType)}
+                </CardTitle>
             </CardHeader>
             <Separator />
             {
                 children ? children : <DefaultCardContent caseData={caseData} />
             }
             <Separator />
-            <CardFooter className="p-0">
+            <CardFooter className="p-0 shrink-0">
                 <Button
                     className="flex text-stone-50 bg-zinc-900 hover:bg-stone-800 rounded-none justify-center flex-grow basis-1/3 max-w-1/2 py-1">
                     <span className="mx-auto text-lg font-bold">Ver</span>
--- frontend/src/components/cases/RecentCases.tsx
+++ frontend/src/components/cases/RecentCases.tsx
@@ -24,7 +24,7 @@ export default function RecentCases() {
         {data.map(c => (
             <CaseCard key={c.id} caseData={c}>
                 <CardContent className="flex flex-col p-2 gap-2 grow">
-                    <p className="text-xl text-stone-200 line-clamp-3 overflow-clip text-ellipsis">
+                    <p className="text-stone-200 line-clamp-3 overflow-clip text-ellipsis">
                         {c.accords[0].content}
                     </p>
                     <p className="text-stone-400 font-semibold mt-auto text-sm">
