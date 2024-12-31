b7744170592460305a5a31732dba75e7f5a9b889
Bladimir Cabrera <vladwithcode@gmail.com>
2024-12-31 14:56:08 -0600

    maintenance: use go's time.Time.Equal function

--- internal/accupdter/general_updater.go
+++ internal/accupdter/general_updater.go
@@ -71,7 +71,7 @@ func (updter *GeneralUpdater) Update(
 	if len(caseKeys) == 0 {
 		return nil, ErrNoCaseKeys
 	}
-	if startSearchDate == (time.Time{}) {
+	if startSearchDate.Equal((time.Time{})) {
 		startSearchDate = updter.conf.SearchStartDate
 	}
 	if maxSearchBack < 0 {
