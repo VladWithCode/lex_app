f32c72c45e88d8277e9354c6f5ede659d6781139
Bladimir Cabrera <vladwithcode@gmail.com>
2025-01-01 01:37:37 -0600

    feat: platform-agnostic database storing
    Setup .db file creation with support for both Windows and Linux.
    TODO: add MacOS support.

--- internal/db/db.go
+++ internal/db/db.go
@@ -5,27 +5,35 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/vladwithcode/lex_app/internal"
 	_ "modernc.org/sqlite"
 )
 
 const DEFAULT_CONN_STR = "data/lex_app.db"
 
 var connStr = os.Getenv("LEX_DB_URL")
+var debug = os.Getenv("LEX_DEBUG") == "true"
 
 var db *sql.DB
 
 func Connect() (db *sql.DB, err error) {
-	if connStr == "" {
+	if debug && connStr == "" {
 		connStr = DEFAULT_CONN_STR
+	} else if !debug {
+		appDir, err := internal.GetAppDataDir()
+		if err != nil {
+			return nil, err
+		}
+		connStr = filepath.Join(appDir, "data/lex_app.db")
+
+		if err := EnsureDBFileExists(connStr); err != nil {
+			return nil, err
+		}
 	}
 
-	db, err = sql.Open("sqlite", "file:"+connStr)
-	if err != nil {
-		return nil, err
-	}
-
-	err = db.Ping()
+	db, err = sql.Open("sqlite", connStr)
 	if err != nil {
 		return nil, err
 	}
@@ -37,6 +45,33 @@ func Connect() (db *sql.DB, err error) {
 	}
 
 	return db, nil
+}
+
+func EnsureDBFileExists(dbPath string) error {
+	_, err := os.Stat(dbPath)
+	if err == nil {
+		return nil
+	}
+
+	if os.IsNotExist(err) {
+		err = os.Mkdir(filepath.Dir(dbPath), 0775)
+		if err != nil && !os.IsExist(err) {
+			return err
+		}
+		f, err := os.Create(dbPath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		err = f.Chmod(0775)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	return err
 }
 
 // Common errors
