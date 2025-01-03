de4bbae0c90b40b7b3df4c3c148e98c07a0dfdf8
Bladimir Cabrera <vladwithcode@gmail.com>
2025-01-02 22:56:19 -0600

    mainetnance: add General FindCasesAndUpdate
    For broad search and update of case accords
    
    Allows the user to search for updates for multiple cases that satisfy the provided search criteria.

--- internal/controllers/accupdtr_controller.go
+++ internal/controllers/accupdtr_controller.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/vladwithcode/lex_app/internal"
 	"github.com/vladwithcode/lex_app/internal/accupdter"
+	"github.com/vladwithcode/lex_app/internal/db"
 	"github.com/vladwithcode/lex_app/internal/fetchers"
 	"github.com/vladwithcode/lex_app/internal/readers"
 )
@@ -42,6 +43,28 @@ func (ctl *AccordUpdaterCtl) FindUpdates(
 }
 
 func (ctl *AccordUpdaterCtl) Update(caseKeys []string, searchStartDate time.Time, maxSearchBack int, exhaustSearch bool) ([]string, error) {
+	return ctl.generalUpdater.Update(caseKeys, searchStartDate, maxSearchBack, exhaustSearch)
+}
+
+func (ctl *AccordUpdaterCtl) FindCasesAndUpdate(
+	searchStartDate time.Time,
+	maxSearchBack int,
+	exhaustSearch bool,
+	findOpts *db.FindCaseOptions,
+) (notFoundKeys []string, err error) {
+	var caseKeys []string
+	var cases []*db.LexCase
+	if findOpts == nil {
+		cases, err = db.FindFilteredCases(ctl.ctx, ctl.appDb, findOpts)
+	} else {
+		cases, err = db.FindAllCases(ctl.ctx, ctl.appDb)
+	}
+	if err != nil {
+		return nil, err
+	}
+	for _, c := range cases {
+		caseKeys = append(caseKeys, c.GetCaseKey())
+	}
 	return ctl.generalUpdater.Update(caseKeys, searchStartDate, maxSearchBack, exhaustSearch)
 }
 
